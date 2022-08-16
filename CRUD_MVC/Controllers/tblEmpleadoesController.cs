00e4be0f1610b377839229856481272ba36e6dc2
Josué Vega <jvega2193@gmail.com>
2022-08-16 16:34:03 -0600

    Commit Inicial

---
+++ CRUD_MVC/Controllers/tblEmpleadoesController.cs
@@ -0,0 +1,128 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUD_MVC;
+
+namespace CRUD_MVC.Controllers
+{
+    public class tblEmpleadoesController : Controller
+    {
+        private EmpresaEntities db = new EmpresaEntities();
+
+        // GET: tblEmpleadoes
+        public ActionResult Index()
+        {
+            return View(db.tblEmpleados.ToList());
+        }
+
+        // GET: tblEmpleadoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblEmpleado tblEmpleado = db.tblEmpleados.Find(id);
+            if (tblEmpleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblEmpleado);
+        }
+
+        // GET: tblEmpleadoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tblEmpleadoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nombres,Apellidos")] tblEmpleado tblEmpleado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblEmpleados.Add(tblEmpleado);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblEmpleado);
+        }
+
+        // GET: tblEmpleadoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblEmpleado tblEmpleado = db.tblEmpleados.Find(id);
+            if (tblEmpleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblEmpleado);
+        }
+
+        // POST: tblEmpleadoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nombres,Apellidos")] tblEmpleado tblEmpleado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblEmpleado).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblEmpleado);
+        }
+
+        // GET: tblEmpleadoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblEmpleado tblEmpleado = db.tblEmpleados.Find(id);
+            if (tblEmpleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblEmpleado);
+        }
+
+        // POST: tblEmpleadoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblEmpleado tblEmpleado = db.tblEmpleados.Find(id);
+            db.tblEmpleados.Remove(tblEmpleado);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
